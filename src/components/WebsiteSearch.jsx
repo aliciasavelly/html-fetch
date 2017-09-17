60caded26252d9260796bcaddcfe329fa67a86a7
aliciasavelly <alicia.savelly@gmail.com>
2017-09-16 18:35:41 -0700

    write method to count tags

--- src/components/WebsiteSearch.jsx
+++ src/components/WebsiteSearch.jsx
@@ -20,13 +20,12 @@ class WebsiteSearch extends Component {
 
     const website = this.state.website;
     const self = this;
-    // debugger;
+
     axios.get(website)
          .then( responseData => {
            const data = responseData.data;
            const parser = new DOMParser();
            const doc = parser.parseFromString(data, "text/html");
-           debugger;
            self.addTags(doc);
          });
   }
@@ -36,6 +35,23 @@ class WebsiteSearch extends Component {
   }
 
   addTags(doc) {
+    let children = doc.children;
+    let len = children.length;
+    if (len <= 0) return;
+    let tags = this.state.tags;
+
+    for (let i = 0; i < len; i++) {
+      let currentTag = children[i].tagName;
+      let stateCopy = Object.assign({}, this.state);
+
+      if (tags[currentTag]) {
+        stateCopy.tags[currentTag] += 1;
+      } else {
+        stateCopy.tags[currentTag] = 1;
+      }
+      this.setState(stateCopy);
+      this.addTags(children[i]);
+    }
     debugger;
   }
 
