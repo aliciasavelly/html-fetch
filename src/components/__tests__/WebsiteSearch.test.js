48b1daa5da80c34ce84f2a7a3933f224d12255cc
aliciasavelly <alicia.savelly@gmail.com>
2017-09-18 14:59:15 -0700

    add more tests

--- src/components/__tests__/WebsiteSearch.test.js
+++ src/components/__tests__/WebsiteSearch.test.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import TagInfo from '../TagInfo';
 import WebsiteSearch from '../WebsiteSearch';
 import { shallow, mount } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 
 describe('WebsiteSearch', () => {
-  it('includes TagInfo component', () => {
-    const component = mount(<WebsiteSearch />);
+  const shallowComponent = shallow(<WebsiteSearch />);
+  const component = mount(<WebsiteSearch />);
 
+  it('includes TagInfo component', () => {
     expect(component.find(TagInfo).length).toBe(1);
-  })
+  });
+
   it('shows website search section', () => {
-    const component = shallow(<WebsiteSearch />);
-    const tree = shallowToJson(component);
+    const tree = shallowToJson(shallowComponent);
 
     expect(tree).toMatchSnapshot();
-  })
+  });
+
+  it('changes website prop when user types in search bar', () => {
+    const websiteInput = component.find('input[type="text"]');
+    websiteInput.simulate('change', { target: { value: 'https://aliciasavelly.github.io/portfolio/' }});
+
+    expect(component.state().website).toEqual('https://aliciasavelly.github.io/portfolio/');
+  });
 });
